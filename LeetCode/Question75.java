7712bf58d8bd40997d21940ee7b3f550ee45cc67
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2024-03-10 12:00:00 +0530

    75lc array question using deatch national flag algo

--- LeetCode/Question75.java
+++ LeetCode/Question75.java
@@ -4,33 +4,34 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Question75 {
-    public static void sortColors(int[] nums) {
-        int n = nums.length;
-        for(int i = 0; i < n-1; i++){
-            int min = i;
-            for(int j = i+1; j<n ; j++){
-                if(nums[j] < nums[min]){
-                    min = j;
-                }
+    public void sortColors(int[] nums) {
+        int mid = 0;
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (mid <= end) {
+            switch (nums[mid]) {
+                case 0:
+                    swap(nums, start, mid);
+                    mid++;
+                    start++;
+                    break;
+
+                case 1:
+                    mid++;
+                    break;
+
+                case 2:
+                    swap(nums, start, end);
+                    end--;
+                    break;
             }
-            //swap
-            int temp = nums[min];
-            nums[min] = nums[i];
-            nums[i] = temp;
-            System.out.println("Pass " + (i + 1) +
-                    " " + Arrays.toString(nums));
         }
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the size of the array: ");
-        int n = sc.nextInt();
-        int[] nums = new int[n];
-        System.out.println("Enter the elements of the array: ");
-        for (int i = 0; i < n; i++) {
-            nums[i] = sc.nextInt();
-        }
-        sortColors(nums);
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
