9a4c1dc7c708e228a66b702cebcb31ad583c45c9
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2024-07-17 22:42:54 +0530

    solve binary search on leetcode Q96

---
+++ LeetCode/Question96.java
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/unique-binary-search-trees/description/
+
+/*Given an integer n, return the number of structurally unique BST's (binary search trees) which has exactly n nodes of unique values from 1 to n. 
+
+Example 1:
+https://assets.leetcode.com/uploads/2021/01/18/uniquebstn3.jpg
+Input: n = 3
+Output: 5
+Example 2:
+
+Input: n = 1
+Output: 1*/
+
+public class Question96 {
+    public static void main(String[] args) {
+        int n = 3;
+        System.out.println(numTrees(n));
+    }
+
+    public static int numTrees(int n) {
+        int[] G = new int[n + 1];
+        G[0] = 1;
+        G[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                G[i] += G[j - 1] * G[i - j];
+            }
+        }
+        return G[n];
+    }
+}
