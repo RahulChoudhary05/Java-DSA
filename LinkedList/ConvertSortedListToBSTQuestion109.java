e1872342cad0e871dd7f6f7fb4b615d188628437
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2024-07-28 12:00:00 +0530

    109lc LinkedList question - Convert Sorted List To BST

--- LinkedList/ConvertSortedListToBSTQuestion109.java
+++ LinkedList/ConvertSortedListToBSTQuestion109.java
@@ -2,34 +2,51 @@ public class ConvertSortedListToBSTQuestion109 {
     public class ListNode {
         int val;
         ListNode next;
-        ListNode() {}
-        ListNode(int val) { this.val = val; }
-        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
-    
+
     public class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
-        TreeNode() {}
-        TreeNode(int val) { this.val = val; }
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
         TreeNode(int val, TreeNode left, TreeNode right) {
             this.val = val;
             this.left = left;
             this.right = right;
         }
     }
+
     public TreeNode sortedListToBST(ListNode head) {
-        if(head == null){
+        if (head == null) {
             return null;
-        } if(head.next == null){
+        }
+        if (head.next == null) {
             return new TreeNode(head.val);
         }
 
         ListNode slow = head;
         ListNode fast = head;
         ListNode slowPrev = null;
-        while(fast != null && fast.next != null){
+        while (fast != null && fast.next != null) {
             slowPrev = slow;
             slow = slow.next;
             fast = fast.next.next;
