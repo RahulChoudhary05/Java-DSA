3aa302cdc3a82bb5eb1f0308dcf7f6df310a6973
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2024-08-04 23:22:34 +0530

    update the code make good complexity

--- SimpleQuestions/FrequencyCounter.java
+++ SimpleQuestions/FrequencyCounter.java
@@ -1,35 +1,28 @@
 //Evaluate the complexity of the developed program , to find the frequency of element in a given array.
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
-public class FrequencyCounter {
+public class Frequency {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the size of the array: ");
-        int n = scanner.nextInt();
 
-        int[] arr = new int[n];
-        System.out.println("Enter the elements of the array:");
-        for (int i = 0; i < n; i++) {
-            arr[i] = scanner.nextInt();
+        System.out.print("Enter the elements of the array (separate by spaces): ");
+        String[] inputElements = scanner.nextLine().split(" ");
+        
+        Map<Integer, Integer> frequencies = new HashMap<>();
+
+        for (int i = 0; i < inputElements.length; i++) {
+            int num = Integer.parseInt(inputElements[i]);
+            frequencies.put(num, frequencies.getOrDefault(num, 0) + 1);
         }
 
-        countAndDisplayFrequency(arr);
-    }
-
-    public static void countAndDisplayFrequency(int[] arr) {
-        Set<Integer> processed = new HashSet<>();
-        for (int i = 0; i < arr.length; i++) {
-            if (!processed.contains(arr[i])) {
-                int count = 1;
-                for (int j = i + 1; j < arr.length; j++) {
-                    if (arr[j] == arr[i]) {
-                        count++;
-                    }
-                }
-                System.out.println(arr[i] + " occurs " + count + " times");
-                processed.add(arr[i]);
-            }
+        System.out.println("Frequency of each element:");
+        for (Map.Entry<Integer, Integer> entry : frequencies.entrySet()) {
+            System.out.println(entry.getKey() + " occurs " + entry.getValue() + " time(s)");
         }
+
+        scanner.close();
     }
 }
