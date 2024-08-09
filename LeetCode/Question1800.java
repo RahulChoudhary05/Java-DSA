80b5553b379d503cbe6459cf3f1ec9a1103b7e89
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2024-08-09 12:00:00 +0530

    1800lc array question Maximum Ascending Subarray Sum

---
+++ LeetCode/Question1800.java
@@ -0,0 +1,18 @@
+package LeetCode;
+
+public class Question1800 {
+    public int maxAscendingSum(int[] nums) {
+        int n = nums.length;
+        int maxSum = nums[0];
+        int currSum = nums[0];
+        for(int i = 1; i < n; i++){
+            if(nums[i] > nums[i-1]){
+                currSum += nums[i];
+            } else{
+                maxSum = Math.max(currSum, maxSum);
+                currSum = nums[i];
+            }
+        }
+        return maxSum = Math.max(currSum, maxSum);
+    }
+}
