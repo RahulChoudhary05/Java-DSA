9c8e19dd4d252f5f5d632a11fec1386a9ef1fbb2
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2024-08-12 12:00:00 +0530

    493lc array question  Optimal Solution: Given an integer array nums, return the number of reverse pairs in the array.

---
+++ LeetCode/Question493.java
@@ -0,0 +1,60 @@
+package LeetCode;
+
+import java.util.ArrayList;
+
+public class Question493 {
+    static int merge(int[] arr, int low, int mid, int high) {
+        ArrayList<Integer> temp = new ArrayList<>();
+        int left = low;      
+        int right = mid + 1;
+
+        int cnt = 0;
+        int j = mid + 1;
+        for (int i = low; i <= mid; i++) {
+            while (j <= high && (long) arr[i] > 2L * arr[j]) {
+                j++;
+            }
+            cnt += (j - (mid + 1));
+        }
+
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp.add(arr[left]);
+                left++;
+            } else {
+                temp.add(arr[right]);
+                right++;
+            }
+        }
+
+        while (left <= mid) {
+            temp.add(arr[left]);
+            left++;
+        }
+
+        while (right <= high) {
+            temp.add(arr[right]);
+            right++;
+        }
+
+        for (int i = low; i <= high; i++) {
+            arr[i] = temp.get(i - low);
+        }
+        return cnt;
+    }
+
+    public static int mergeSort(int[] arr, int low, int high) {
+        int cnt = 0;
+        if (low >= high) return cnt;
+        int mid = (low + high) / 2;
+        cnt += mergeSort(arr, low, mid);    
+        cnt += mergeSort(arr, mid + 1, high);
+        cnt += merge(arr, low, mid, high);
+        return cnt;
+    }
+
+    static int reversePairs(int nums[]) {
+        int n = nums.length;
+        return mergeSort(nums, 0, n - 1);
+    }
+}
