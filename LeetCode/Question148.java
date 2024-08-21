64bdf66149ef5a324b5ca1c85c388d6c25c5e257
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2024-08-21 12:00:00 +0530

    solve a question on leetcode 148 about sortlinklist

---
+++ LeetCode/Question148.java
@@ -0,0 +1,99 @@
+// image -  https://assets.leetcode.com/uploads/2020/09/14/sort_list_2.jpg
+
+/*148. Sort List
+Given the head of a linked list, return the list after sorting it in ascending order.
+
+Example 1:
+Input: head = [4,2,1,3]
+Output: [1,2,3,4]
+
+Example 2:
+Input: head = [-1,5,3,4,0]
+Output: [-1,0,3,4,5]
+Follow up: Can you sort the linked list in O(n logn) time and O(1) memory (i.e. constant space)?*/
+
+package LeetCode;
+
+public class Question148 {
+    /**
+     * Definition for singly-linked list.
+     */
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode mid = middleNode(head);
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+        return merge(left, right);
+    }
+
+    public ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(-1);
+        ListNode current = dummyHead;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                current.next = l1;
+                l1 = l1.next;
+            } else {
+                current.next = l2;
+                l2 = l2.next;
+            }
+            current = current.next;
+        }
+        current.next = (l1 != null) ? l1 : l2;
+        return dummyHead.next;
+    }
+
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        if (prev != null) {
+            prev.next = null;
+        }
+
+        return slow;
+    }
+
+     public static void main(String[] args) {
+        // Create a sample linked list to test the sortList method
+        ListNode head1 = new ListNode(4, new ListNode(2, new ListNode(1, new ListNode(3))));
+        ListNode head2 = new ListNode(-1, new ListNode(5, new ListNode(3, new ListNode(4, new ListNode(0)))));
+
+        // Create an instance of Question148
+        Question148 q = new Question148();
+
+        // Test the sortList method
+        ListNode sorted1 = q.sortList(head1);
+        ListNode sorted2 = q.sortList(head2);
+
+        // Print the sorted results
+        printList(sorted1);
+        printList(sorted2);
+    }
+
+    // Helper method to print the linked list
+    public static void printList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
