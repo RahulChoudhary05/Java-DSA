6eef9434a38043c4088acb323d616b03b6731258
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2024-09-12 00:19:55 +0530

    add the alphabet code

--- Pattern/StarPattern.java
+++ Pattern/StarPattern.java
@@ -3,13 +3,32 @@ import java.util.*;
 public class StarPattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a rows number: ");
-        int number = sc.nextInt();
-        forNumber(number);
+        //for number
+        // System.out.print("Enter a rows number: ");
+        // int number = sc.nextInt();
+        // forNumber(number);
+
+        //for alphabet char
+        System.out.print("Enter the first char: ");
+        char firstChar = sc.next().charAt(0);
+        System.out.print("Enter the Second char: ");
+        char secondChar = sc.next().charAt(0);
+        forChar(firstChar, secondChar);
     }
-    public static int forNumber(int number){
-        for (int i = 1; i <= number; i++) {
-            for (int j = 1; j <= number; j++) { // Inner loop for one line
+
+    // public static int forNumber(int number){
+    //     for (int i = 1; i <= number; i++) {
+    //         for (int j = 1; j <= number; j++) {
+    //             System.out.print(j);
+    //         }
+    //         System.out.println();
+    //     }
+    //     return 0;
+    // }
+
+    public static char forChar(char firstChar, char secondChar){
+        for (char i = firstChar; i <= secondChar; i++) {
+            for (char j = firstChar; j <= secondChar; j++) {
                 System.out.print(j);
             }
             System.out.println();
