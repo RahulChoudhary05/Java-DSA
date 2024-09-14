fd473cc1a9d495f3e2e6502e52d62c6a48511446
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2024-09-14 17:51:48 +0530

    done with Selection sord simple question

--- Sorting/SelectionSort/SelectionSort.java
+++ Sorting/SelectionSort/SelectionSort.java
@@ -1,23 +1,11 @@
 package Sorting.SelectionSort;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SelectionSort {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the size of the array: ");
-        int size = sc.nextInt();
-        int[] arr = new int[size];
-        System.out.println("Enter the elements of the array: ");
-        for (int i = 0; i < size; i++) {
-            arr[i] = sc.nextInt();
-        }
-        selectionsortcode(arr);
-    }
-
-    public static void selectionsortcode(int arr[]) {
-        int n = arr.length;
-        for (int i = 0; i < n - 1; i++) {
+    public static void selectionsortcode(int arr[], int n) {
+        for (int i = 0; i < n - 2; i++) {
             int minIndex = i;
             for (int j = i + 1; j < n; j++) {
                 if (arr[j] < arr[minIndex]) {
@@ -27,12 +15,20 @@ public class SelectionSort {
             int temp = arr[minIndex];
             arr[minIndex] = arr[i];
             arr[i] = temp;
-
-            System.out.print("After pass " + (i + 1) + ": ");
-            for (int k = 0; k < n; k++) {
-                System.out.print(arr[k] + " ");
-            }
-            System.out.println();
+            System.out.println("Pass " + (i + 1) +
+                    " " + Arrays.toString(arr));
         }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        selectionsortcode(arr, n);
     }
 }
