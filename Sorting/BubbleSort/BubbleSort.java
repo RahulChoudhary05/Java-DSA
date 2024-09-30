6d4b72cbc93a5b667e860086595977cf1bc64f29
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2024-09-30 18:50:07 +0530

    fix the bubble sort j=0

--- Sorting/BubbleSort/BubbleSort.java
+++ Sorting/BubbleSort/BubbleSort.java
@@ -5,8 +5,9 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public static void bubblesortcode(int arr[], int n) {
+        n = arr.length;
         for (int i = 0; i < n-1; i++) {
-            for (int j = 1; j < n-i-1; j++) {
+            for (int j = 0; j < n-i-1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     // swap
                     int temp = arr[j];
@@ -14,7 +15,7 @@ public class BubbleSort {
                     arr[j+1] = temp;
                 }
             }
-            System.out.println("Pass " + (i) + " " + Arrays.toString(arr));
+            System.out.println("Pass " + i + " " + Arrays.toString(arr));
         }
     }
 
