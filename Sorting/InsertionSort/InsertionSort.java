e5825e311b0a38a645303ab02d449276b8c44832
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2024-10-01 19:10:18 +0530

    done with insertion sort

--- Sorting/InsertionSort/InsertionSort.java
+++ Sorting/InsertionSort/InsertionSort.java
@@ -13,7 +13,7 @@ public class InsertionSort {
         insertionSortCode(arr, n);
     }
     public static void insertionSortCode(int arr[], int n) {
-        for (int i = 1; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             int temp = arr[i];
             int j = i - 1;
             while (j >= 0 && arr[j] > temp) {
