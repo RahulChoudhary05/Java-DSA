143108d5b89e788296b69a982008f0f061ecc1e4
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2024-10-02 12:00:00 +0530

    121lc array question stock buy and sell

---
+++ LeetCode/Question121.java
@@ -0,0 +1,15 @@
+public class Question121 {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int buy = prices[0];
+        for(int i = 0; i < prices.length; i++){
+            int sell = prices[i];
+            if(sell > buy){
+                profit = Math.max(profit, sell - buy);
+            } else{
+                buy = sell;
+            }
+        }
+        return profit;
+    }
+}
