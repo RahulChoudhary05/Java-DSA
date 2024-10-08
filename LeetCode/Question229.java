2097e90c74781a117295ab703301e1313791902c
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2024-10-08 12:00:00 +0530

    229lc array question Majority Element II

--- LeetCode/Question229.java
+++ LeetCode/Question229.java
@@ -1,25 +1,22 @@
 package LeetCode;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Question229 {
+    public List<Integer> majorityElement(int[] nums) {
+        int n = nums.length;
+        List<Integer> list = new ArrayList<>();
+        int mm = n / 3;
+        HashMap<Integer, Integer> mapp = new HashMap<>();
 
+        for (int i = 0; i < n; i++) {
+            mapp.put(nums[i], mapp.getOrDefault(nums[i], 0) + 1);
+            if (mapp.get(nums[i]) == mm + 1 && !list.contains(nums[i])) {
+                list.add(nums[i]);
+            }
+        }
+        return list;
+    }
 }
-
-/*
- * - Better solution:
- * class Solution {
- * public List<Integer> majorityElement(int[] nums) {
- * int n = nums.length;
- * List<Integer> list = new ArrayList<>();
- * int mm = n / 3;
- * HashMap<Integer, Integer> mapp = new HashMap<>();
- * 
- * for (int i = 0; i < n; i++) {
- * mapp.put(nums[i], mapp.getOrDefault(nums[i], 0) + 1);
- * if (mapp.get(nums[i]) == mm + 1 && !list.contains(nums[i])) {
- * list.add(nums[i]);
- * }
- * }
- * return list;
- * }
- * }
- */
