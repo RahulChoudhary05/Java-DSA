87a16bbaf4692c630883776b5fde7622faef622b
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2025-01-19 12:00:00 +0530

    array questions 136lc single number

---
+++ LeetCode/Question136.java
@@ -0,0 +1,31 @@
+package LeetCode;
+
+public class Question136 {
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            ans = ans ^ nums[i];
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Question136 question = new Question136();
+        
+        // Example 1
+        int[] nums1 = {2, 2, 1};
+        System.out.println("Input: nums = [2, 2, 1]");
+        System.out.println("Output: " + question.singleNumber(nums1)); 
+        
+        // Example 2
+        int[] nums2 = {4, 1, 2, 1, 2};
+        System.out.println("Input: nums = [4, 1, 2, 1, 2]");
+        System.out.println("Output: " + question.singleNumber(nums2));
+
+        // Example 3
+        int[] nums3 = {1};
+        System.out.println("Input: nums = [1]");
+        System.out.println("Output: " + question.singleNumber(nums3));
+    }
+}
