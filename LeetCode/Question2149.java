3a152981a865f3081103d8e31fecff4311b14e58
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2025-01-25 18:41:19 +0530

    Rearrange Array Elements by Sign

--- LeetCode/Question2149.java
+++ LeetCode/Question2149.java
@@ -11,7 +11,7 @@ public class Question2149 {
                 negativeIndex += 2;
             } else {
                 ans[positiveIndex] = nums[i];
-                positiveIndex += 2;
+                positiveIndex += 1;
             }
         }
         return ans;
