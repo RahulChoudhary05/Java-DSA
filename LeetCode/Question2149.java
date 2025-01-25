c8a55379f8a81a3b560ac59556cc144721f1de2b
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2025-01-25 18:40:55 +0530

    Rearrange Array Elements by Sign

---
+++ LeetCode/Question2149.java
@@ -0,0 +1,19 @@
+public class Question2149 {
+    public int[] rearrangeArray(int[] nums) {
+        int n = nums.length;
+        int positiveIndex = 0;
+        int negativeIndex = 1;
+        int[] ans = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < 0) {
+                ans[negativeIndex] = nums[i];
+                negativeIndex += 2;
+            } else {
+                ans[positiveIndex] = nums[i];
+                positiveIndex += 2;
+            }
+        }
+        return ans;
+    }
+}
