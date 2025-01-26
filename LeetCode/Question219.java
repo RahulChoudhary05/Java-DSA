30d565608ad11ffd1aa9038445a6a71a031a9606
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2025-01-26 20:46:17 +0530

    219lc array containsNearbyDuplicate

---
+++ LeetCode/Question219.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+public class Question219 {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> indexMap = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (indexMap.containsKey(nums[i])) {
+                int lastIndex = indexMap.get(nums[i]);
+                if (i - lastIndex <= k) {
+                    return true;
+                }
+            }
+            indexMap.put(nums[i], i);
+        }
+        
+        return false;
+    }
+}
