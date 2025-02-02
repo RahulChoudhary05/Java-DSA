61bf41ba1a0bf74563748812943c594f881e288e
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2025-02-02 21:27:58 +0530

    Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
    Each number in candidates may only be used once in the combination.

---
+++ LeetCode/Question40.java
@@ -0,0 +1,35 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Question40 {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(candidates);
+        backtrack(result, new ArrayList<>(), candidates, target, 0);
+        return result;
+    }
+
+    private void backtrack(List<List<Integer>> result, List<Integer> combination, 
+                           int[] candidates, int target, int start) {
+        
+        if (target == 0) {
+            result.add(new ArrayList<>(combination));
+            return;
+        }
+        
+        for (int i = start; i < candidates.length; i++) {
+            if (candidates[i] > target) {
+                break;
+            }
+            if (i > start && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
+            combination.add(candidates[i]);
+            backtrack(result, combination, candidates, target - candidates[i], i + 1);
+            combination.remove(combination.size() - 1);
+        }
+    }
+}
