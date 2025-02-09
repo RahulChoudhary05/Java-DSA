0b51a00bd2366a2d0870a8777cc9d8bd1a7a4a78
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2025-02-09 12:00:00 +0530

    796lc string question rotateString

---
+++ String/Question796.java
@@ -0,0 +1,10 @@
+package String;
+
+public class Question796 {
+    public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        return (s + s).contains(goal);
+    }
+}
