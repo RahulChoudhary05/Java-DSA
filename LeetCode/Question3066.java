454a261a81d7e7d0121dcdf769e7392665a78994
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2025-02-13 19:13:10 +0530

    3066lc array question Minimum Operations to Exceed Threshold Value II

---
+++ LeetCode/Question3066.java
@@ -0,0 +1,21 @@
+package LeetCode;
+
+public class Question3066 {
+    public int minOperations(int[] nums, int k) {
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+
+        for(int i = 0; i < nums.length; i++){
+            pq.add((long) nums[i]);
+        }
+        int count = 0;
+
+        while(!pq.isEmpty() && pq.peek() < k){
+            long firstSmallNo = pq.poll();
+            long secondSmallNo = pq.poll();
+
+            pq.add(firstSmallNo * 2 + secondSmallNo);
+            count++;
+        }
+        return count;
+    }
+}
