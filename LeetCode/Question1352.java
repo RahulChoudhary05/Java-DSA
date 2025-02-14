4aaa30ab755fd81ca930c90b18690437d033efaf
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2025-02-14 18:50:06 +0530

    1352lc Product of the Last K Numbers

--- LeetCode/Question1352.java
+++ LeetCode/Question1352.java
@@ -11,27 +11,27 @@ public class Question1352 {
         n = 0;
         nums = new ArrayList<>();
     }
-    
+
     public void add(int num) {
-        if(num == 0 ){
+        if (num == 0) {
             nums.clear();
             n = 0;
-        } else{
-            if(nums.isEmpty()){
+        } else {
+            if (nums.isEmpty()) {
                 nums.add(num);
-            } else{
-                nums.add(nums.get(n-1) * num);
+            } else {
+                nums.add(nums.get(n - 1) * num);
             }
             n++;
         }
     }
-    
+
     public int getProduct(int k) {
-        if(n < k){
+        if (n < k) {
             return 0;
-        } else if(n == k){
-            return nums.get(n-1);
+        } else if (n == k) {
+            return nums.get(n - 1);
         }
-        return nums.get(n-1) / nums.get(n-k-1);
+        return nums.get(n - 1) / nums.get(n - k - 1);
     }
 }
