1999cab2999a9c0080eba34f2e1ac424f58e8ca7
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2025-02-14 19:46:59 +0530

    706lc hash question - Design a HashMap without using any built-in hash table libraries.

--- HashTable/HashSet/Question706.java
+++ HashTable/HashSet/Question706.java
@@ -4,99 +4,99 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Question706 {
-    //Pair
-public class Pair<U, V> {
-    U first;
-    V second;
+    // Pair
+    public class Pair<U, V> {
+        U first;
+        V second;
 
-    public Pair(U f, V s) {
-        first = f;
-        second = s;
-    }
-}
-
-//Bucket
-class Bucket {
-    List<Pair<Integer, Integer>> list;
-
-    public Bucket() {
-        list = new LinkedList<Pair<Integer, Integer>>(); 
+        public Pair(U f, V s) {
+            first = f;
+            second = s;
+        }
     }
 
-    public void put(int key, int value) {
-        // update the value by its key
-        for (Pair<Integer, Integer> p : list) {
-            if (p.first.equals(key)) {
-                p.second = value;
-                return;
+    // Bucket
+    class Bucket {
+        List<Pair<Integer, Integer>> list;
+
+        public Bucket() {
+            list = new LinkedList<Pair<Integer, Integer>>();
+        }
+
+        public void put(int key, int value) {
+            // update the value by its key
+            for (Pair<Integer, Integer> p : list) {
+                if (p.first.equals(key)) {
+                    p.second = value;
+                    return;
+                }
+            }
+            list.add(new Pair<Integer, Integer>(key, value));
+        }
+
+        public int get(int key) {
+            // update the value by its key
+            for (Pair<Integer, Integer> p : list) {
+                if (p.first.equals(key)) {
+                    return p.second;
+                }
+            }
+            // if don't exist
+            return -1;
+        }
+
+        public void remove(int key) {
+            // update the value by its key
+            for (Pair<Integer, Integer> p : list) {
+                if (p.first.equals(key)) {
+                    list.remove(p);
+                    return;
+                }
             }
         }
-        list.add(new Pair<Integer, Integer>(key, value));
     }
 
-    public int get(int key) {
-        // update the value by its key
-        for (Pair<Integer, Integer> p : list) {
-            if (p.first.equals(key)) {
-                return p.second;
+    class MyHashMap {
+        int keyRange = 769;
+        Bucket[] buckets;
+
+        public MyHashMap() {
+            this.buckets = new Bucket[this.keyRange];
+
+            for (int i = 0; i < keyRange; i++) {
+                this.buckets[i] = new Bucket();
             }
         }
-        // if don't exist
-        return -1;
-    }
 
-    public void remove(int key) {
-        // update the value by its key
-        for (Pair<Integer, Integer> p : list) {
-            if (p.first.equals(key)) {
-                list.remove(p);
-                return;
-            }
+        public int getBucketKeyIndex(int key) {
+            return key % keyRange;
         }
-    }
-}
 
-class MyHashMap {
-    int keyRange = 769;
-    Bucket[] buckets;
+        public void put(int key, int value) {
+            // take bucket(by its index - getBucketKeyIndex)
+            int bucketIndex = this.getBucketKeyIndex(key);
+            this.buckets[bucketIndex].put(key, value);
+        }
 
-    public MyHashMap() {
-        this.buckets = new Bucket[this.keyRange];
+        public int get(int key) {
+            // take bucket(by its index - getBucketKeyIndex)
+            int bucketIndex = this.getBucketKeyIndex(key);
+            return this.buckets[bucketIndex].get(key);
+        }
 
-        for (int i = 0; i < keyRange; i++) {
-            this.buckets[i] = new Bucket();
+        public void remove(int key) {
+            // take bucket(by its index - getBucketKeyIndex)
+            int bucketIndex = this.getBucketKeyIndex(key);
+            this.buckets[bucketIndex].remove(key);
         }
     }
 
-    public int getBucketKeyIndex(int key) {
-        return key % keyRange;
-    }
-    
-    public void put(int key, int value) {
-        // take bucket(by its index - getBucketKeyIndex)
-        int bucketIndex = this.getBucketKeyIndex(key);
-        this.buckets[bucketIndex].put(key, value);
-    }
-    
-    public int get(int key) {
-        // take bucket(by its index - getBucketKeyIndex)
-        int bucketIndex = this.getBucketKeyIndex(key);
-        return this.buckets[bucketIndex].get(key);
-    }
-    
-    public void remove(int key) {
-        // take bucket(by its index - getBucketKeyIndex)
-        int bucketIndex = this.getBucketKeyIndex(key);
-        this.buckets[bucketIndex].remove(key);
-    }
-}
-
-/**
- * Your MyHashMap object will be instantiated and called as such:
- * MyHashMap obj = new MyHashMap();
- * obj.put(key,value);
- * int param_2 = obj.get(key);
- * obj.remove(key);
- */
+    /**
+     * Your MyHashMap object will be instantiated and called as such:
+     * MyHashMap obj = new MyHashMap();
+     * obj.put(key,value);
+     * int param_2 = obj.get(key);
+     * obj.remove(key);
+     */
 
 }
