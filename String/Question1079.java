278f31e485d8278434d3d36f663defd5b5ebf60c
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2025-02-17 21:46:22 +0530

    1079lc string question numTilePossibilities

---
+++ String/Question1079.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    int count = 0;
+
+    public int numTilePossibilities(String tiles) {
+        Map<Character, Integer> charCount = new HashMap<>();
+        
+        for (int i = 0; i < tiles.length(); i++) {
+            char c = tiles.charAt(i);
+            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        }
+
+        backtrack(charCount, 0);
+        return count;
+    }
+
+    private void backtrack(Map<Character, Integer> charCount, int length) {
+        if (length > 0) {
+            count++;
+        }
+
+        for (int i = 'A'; i <= 'Z'; i++) {
+            char c = (char)i;
+            if (charCount.containsKey(c) && charCount.get(c) > 0) {
+                charCount.put(c, charCount.get(c) - 1);
+                backtrack(charCount, length + 1);
+                charCount.put(c, charCount.get(c) + 1);
+            }
+        }
+    }
+}
