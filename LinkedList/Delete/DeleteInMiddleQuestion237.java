ae5de3cb98422addaac6b5d4fe10a102fef82c0d
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2025-02-18 20:01:05 +0530

    237lc Linked List question Delete Node in a Linked List

--- LinkedList/Delete/DeleteInMiddleQuestion237.java
+++ LinkedList/Delete/DeleteInMiddleQuestion237.java
@@ -1,31 +1,33 @@
 package LinkedList.Delete;
 
-
 public class DeleteInMiddleQuestion237 {
 
- public class ListNode {
-     int val;
-     ListNode next;
-     ListNode(int x) { val = x; }
- }
- 
-class Solution {
-    public void deleteNode(ListNode node) {
-        ListNode prev = null;
+    public class ListNode {
+        int val;
+        ListNode next;
 
-        while(node != null && node.next != null){
-            node.val = node.next.val;
-            prev = node;
-            node = node.next;
+        ListNode(int x) {
+            val = x;
         }
+    }
 
-        if(node != null){
-            if(prev != null){
-                prev.next = null;
-            } else{
-                node = null;
+    class Solution {
+        public void deleteNode(ListNode node) {
+            ListNode prev = null;
+
+            while (node != null && node.next != null) {
+                node.val = node.next.val;
+                prev = node;
+                node = node.next;
+            }
+
+            if (node != null) {
+                if (prev != null) {
+                    prev.next = null;
+                } else {
+                    node = null;
+                }
             }
         }
     }
-}
 }
