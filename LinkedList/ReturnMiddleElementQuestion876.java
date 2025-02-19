411fc8d95d7dba508165d8d594f71416f5aefa4e
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2025-02-19 20:22:59 +0530

    876lc Linked List question Given the head of a singly linked list, return the middle node of the linked list.

---
+++ LinkedList/ReturnMiddleElementQuestion876.java
@@ -0,0 +1,35 @@
+package LinkedList;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class ReturnMiddleElementQuestion876 {
+    public ListNode middleNode(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
