88f6114c5135ef21338195178a2cb68368e5b906
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2025-02-20 12:00:00 +0530

    382lc LinkedList question - Random Node

--- LinkedList/RandomNodeQuestion382.java
+++ LinkedList/RandomNodeQuestion382.java
@@ -3,26 +3,27 @@ package LinkedList;
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 public class RandomNodeQuestion382 {
     ListNode temp;
+
     public RandomNodeQuestion382(ListNode head) {
         temp = head;
     }
-    
+
     public int getRandom() {
         int result = 0;
         ListNode tempArray = temp;
         int count = 1;
-        while(tempArray != null){
-            if(Math.random() < 1.0 / count){
-            result = tempArray.val;
+        while (tempArray != null) {
+            if (Math.random() < 1.0 / count) {
+                result = tempArray.val;
             }
             count++;
             tempArray = tempArray.next;
@@ -30,9 +31,9 @@ public class RandomNodeQuestion382 {
         return result;
     }
 
-/**
- * Your Solution object will be instantiated and called as such:
- * Solution obj = new Solution(head);
- * int param_1 = obj.getRandom();
- */
+    /**
+     * Your Solution object will be instantiated and called as such:
+     * Solution obj = new Solution(head);
+     * int param_1 = obj.getRandom();
+     */
 }
