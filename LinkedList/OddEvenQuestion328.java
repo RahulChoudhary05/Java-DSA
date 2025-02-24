049bf88e08f697ee0197dd6e223011841ba97813
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2025-02-24 19:15:34 +0530

    328lc LinkedList question - Odd Even Linked List

---
+++ LinkedList/OddEvenQuestion328.java
@@ -0,0 +1,34 @@
+package LinkedList;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+public class OddEvenQuestion328 {
+    public ListNode oddEvenList(ListNode head) {
+        if(head == null || head.next == null || head.next.next == null){ 
+            return head; 
+        }
+
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenStart = head.next;
+
+        while(even != null && even.next != null){
+            odd.next = even.next;
+            even.next = even.next.next;
+
+            odd = odd.next;
+            even = even.next;
+            odd.next = evenStart;
+        }
+        return head;
+    }
+}
