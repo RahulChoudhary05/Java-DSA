f0c918ea39cb6dc7aaa750b80f9913ba46fa9403
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2025-03-11 17:01:54 +0530

    142lc Linked List question - (new best logic) Linked List Cycle II

--- LinkedList/LinkedListCycleIIQuestion142.java
+++ LinkedList/LinkedListCycleIIQuestion142.java
@@ -1,16 +1,15 @@
 package LinkedList;
 
-/**
- * Definition for singly-linked list.
- * class ListNode {
- * int val;
- * ListNode next;
- * ListNode(int x) {
- * val = x;
- * next = null;
- * }
- * }
- */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
 public class LinkedListCycleIIQuestion142 {
     public ListNode detectCycle(ListNode head) {
         if (head == null || head.next == null) {
@@ -39,3 +38,40 @@ public class LinkedListCycleIIQuestion142 {
         return slow;
     }
 }
+
+/* another best solution
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if(head == null || head.next == null){
+            return null;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if(slow == fast){
+                slow = head;
+                while(slow != fast){
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
+*/
