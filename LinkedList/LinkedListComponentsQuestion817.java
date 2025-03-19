f67bf7599af6cb1dc95965751fe77fb24421dc4c
Rahul Choudhary <rahulchoudhary.sk@gmail.com>
2025-03-19 14:33:53 +0530

    817lc Linked List question -  Linked List Components

---
+++ LinkedList/LinkedListComponentsQuestion817.java
@@ -0,0 +1,43 @@
+package LinkedList;
+
+class LinkedListComponentsQuestion817 {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public int numComponents(ListNode head, int[] nums) {
+        Set<Integer> map = new HashSet<>();
+
+        for (int i : nums) {
+            map.add(i);
+        }
+        int count = 0;
+        boolean current = false;
+        ListNode temp = head;
+        while (temp != null) {
+            if (map.contains(temp.val)) {
+                if (!current) {
+                    count++;
+                    current = true;
+                }
+            } else {
+                current = false;
+            }
+            temp = temp.next;
+        }
+        return count;
+    }
+}
